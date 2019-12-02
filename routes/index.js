1ad1a2fc9c9afaf87227416b09af89506a4b78ce
MagicZL0 <tnwls9721@naver.com>
2019-12-02 11:56:18 +0900

    날짜 기능구현

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//날짜
+router.get('/date', function(req, res, next)
+{
+  var date = new Date();
+  if(date != undefined) //undefined : 객체가 생성되지 않은 경우
+  {
+    var dateObj = { date : date };
+    res.status(200).send(dateObj);
+  }
+});
+
 module.exports = router;
